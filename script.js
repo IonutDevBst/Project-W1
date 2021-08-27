3a996469ca69c2575e419bcfee33f5f7447258a3
ionutdev <aritonionutz@yahoo.com>
2021-08-27 17:13:30 +0300

    another commit9

--- script.js
+++ script.js
@@ -26,6 +26,10 @@ var modal2 = document.getElementById('id02');
 var modal3 = document.getElementById('id03');
 var modal4 = document.getElementById('id04');
 var modal5 = document.getElementById('id05');
+var modal6 = document.getElementById('id06');
+var modal7 = document.getElementById('id07');
+var modal8 = document.getElementById('id08');
+var modal9 = document.getElementById('id09');
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
@@ -39,6 +43,14 @@ window.onclick = function(event) {
         modal4.style.display = "none";
     } else if (event.target == modal5) {
         modal5.style.display = "none";
+    } else if (event.target == modal6) {
+        modal6.style.display = "none";
+    } else if (event.target == modal7) {
+        modal7.style.display = "none";
+    } else if (event.target == modal8) {
+        modal8.style.display = "none";
+    } else if (event.target == modal9) {
+        modal9.style.display = "none";
     }
 }
 
